aac69b4dcede524a43ee8d1acfb48bd0397d4669
jmoze <jonathan.moze@etu.u-bordeaux.fr>
2021-01-09 00:34:33 +0100

    possibilité suivre+ne plus suivre serie

--- src/Controller/UserSpaceController.php
+++ src/Controller/UserSpaceController.php
@@ -3,19 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Series;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class UserSpaceController extends AbstractController
 {
     /**
-     * @Route("/userSpace", name="user_space")
+     * @Route("/mes_series", name="mes_series")
      */
-    public function index(): Response
+    public function mes_series(Request $requete, PaginatorInterface $paginator): Response
     {
-        return $this->render('user_space/index.html.twig', [
-            'controller_name' => 'UserSpaceController',
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+
+
+        $mesSeries = $user->getSeries()
+        ;
+        $series=$paginator->paginate(
+            $mesSeries,
+            $requete->query->getInt('page',1),
+            10
+        );
+
+        return $this->render('user_space/mes_series.html.twig', [
+            'series' => $series,
         ]);
     }
 
@@ -33,6 +47,22 @@ class UserSpaceController extends AbstractController
         $entityManager->persist($user);
         $entityManager->flush();
         
-        return $this->redirectToRoute('home');
+        return $this->redirectToRoute('series');
+    }
+
+    /**
+     * @Route("/user/removeSerie/{id}", name="remove_serie")
+     */
+    public function remove_serie(Series $serie)
+    {
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $user->removeSeries($serie);
+
+        
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('series');
     }
 }
